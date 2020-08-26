e68fdc0ed07886cc0eb14c55f4a108617879d7ec
patty175 <68931639+patty175@users.noreply.github.com>
2020-08-25 22:54:13 -0300

    Update

--- js/products.js
+++ js/products.js
@@ -50,8 +50,6 @@ function showProductsList(array){
             ((maxCount == undefined) || (maxCount != undefined && parseInt(category.soldCount) <= maxCount)) &&
                 ( (productDescription.indexOf(text)) !== -1 || (productName.indexOf(text)) !== -1) 
             ){
-        if (((minCount == undefined) || (minCount != undefined && parseInt(category.cost) >= minCount)) &&
-            ((maxCount == undefined) || (maxCount != undefined && parseInt(category.cost) <= maxCount))){
 
         htmlContentToAppend += '<div class="list-group-item list-group-item-action">\
         <div class="row"  onclick="showSpinner()">\
@@ -81,8 +79,7 @@ function showProductsList(array){
     
         productsList = sortByPrice(currentSortCriteria, productsList);
     
-        
-        //Muestro los autos ordenadas
+        //Muestro los autos ordenados
         showProductsList();
     }
 
@@ -157,48 +154,3 @@ function busqueda() {
 document.getElementById("myText").addEventListener("keyup", busqueda);
 busqueda();
 
-
-    document.getElementById("priceDesc").addEventListener("click", function(){
-        sortAndShowCars(ORDER_DESC_BY_PRICE);
-    });
-
-    document.getElementById("sortByCount").addEventListener("click", function(){
-        sortAndShowCars(ORDER_BY_PROD_COUNT);
-    });
-
-    document.getElementById("rangeFilterCount").addEventListener("click", function(){
-        //Obtengo el mínimo y máximo del valor de los productos
-        minCount = document.getElementById("rangeFilterCountMin").value;
-        maxCount = document.getElementById("rangeFilterCountMax").value;
-
-        console.log("hola")
-        if ((minCount != undefined) && (minCount != "") && (parseInt(minCount)) >= 0){
-            minCount = parseInt(minCount);
-        }
-        else{
-            minCount = undefined;
-        }
-
-        if ((maxCount != undefined) && (maxCount != "") && (parseInt(maxCount)) >= 0){
-            maxCount = parseInt(maxCount);
-        }
-        else{
-            maxCount = undefined;
-        }
-
-        showProductsList();
-    });
-
-    document.getElementById("clearRangeFilter").addEventListener("click", function(){
-      document.getElementById("rangeFilterCountMin").value = "";
-      document.getElementById("rangeFilterCountMax").value = "";
-
-    
-      minCount = undefined;
-      maxCount = undefined;
-
-        showProductsList();
-    
-    });
-
-});
